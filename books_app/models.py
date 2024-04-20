f0219f2a8d383826efae02b4ec27959e9d358dd2
numer <5079447@gmail.com>
2024-04-20 22:52:23 +0300

    Implemented "Book" model.

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices,
+        default=CoverChoices.HARD,
+    )
+    inventory = models.PositiveSmallIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together ("title", "author")
+
+    def __str__(self):
+        return f"{self.title} - {self.author}, available - {self.inventory}"
