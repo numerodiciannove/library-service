10530ddba61e7f2ae5bd668644b5337a40bd2eef
numer <5079447@gmail.com>
2024-04-21 00:19:01 +0300

    Implemented full CRUD API views for books.

--- books_app/views.py
+++ books_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from books_app.serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
+from books_app.models import Book
 
-# Create your views here.
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
