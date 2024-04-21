826021bc6a6ef62fe0fbf197508b9ae7a8ec11a7
numer <5079447@gmail.com>
2024-04-21 03:54:29 +0300

    Updated models

--- users_app/models.py
+++ users_app/models.py
@@ -41,10 +41,10 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
     username = None
     email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
