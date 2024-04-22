0c1c7d10597342a50cf282a77ee8a52a919e4eae
numer <5079447@gmail.com>
2024-04-22 15:14:19 +0300

    Added "BorrowingViewSet" for retrieve & list implementation.

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from borrowings_app.models import Borrowing
+from borrowings_app.serializers import BorrowSerializer, BorrowRetrieveSerializer
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+
+class BorrowingViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowRetrieveSerializer
+
+        return BorrowSerializer
