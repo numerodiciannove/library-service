3ba24f8d274ef12902f87532751bb0a5523d4729
numer <5079447@gmail.com>
2024-04-22 19:49:30 +0300

    Implemented create view for borrowings.

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -1,19 +1,20 @@
+from rest_framework.viewsets import ModelViewSet
 from borrowings_app.models import Borrowing
-from borrowings_app.serializers import BorrowSerializer, BorrowRetrieveSerializer
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from borrowings_app.serializers import (
+    BorrowSerializer,
+    BorrowRetrieveSerializer,
+    BorrowCreateSerializer,
+)
 
 
-class BorrowingViewSet(
-    mixins.RetrieveModelMixin,
-    mixins.ListModelMixin,
-    GenericViewSet,
-):
+class BorrowingViewSet(ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowSerializer
 
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowRetrieveSerializer
+        if self.action == "create":
+            return BorrowCreateSerializer
 
         return BorrowSerializer
