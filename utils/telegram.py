b179c54bbc79ef522d5134acf2050bdf7ed88d85
numer <5079447@gmail.com>
2024-04-25 00:00:47 +0300

    Implemented telegram message sender.

---
+++ utils/telegram.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def message_sender(text_message: str) -> [dict, list]:
+    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
+    telegram_method = "sendMessage"
+
+    response = requests.post(
+        url="https://api.telegram.org/bot{0}/{1}".format(
+            telegram_bot_token, telegram_method
+        ),
+        data={"chat_id": telegram_chat_id, "text": text_message}
+    ).json()
+
+    return response
