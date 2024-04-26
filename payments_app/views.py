c5738bbfc9d300311af57e6cceb7245d1d753941
numer <5079447@gmail.com>
2024-04-26 19:01:53 +0300

    implemented List & Detail Payments Endpoint.

--- payments_app/views.py
+++ payments_app/views.py
@@ -1,14 +1,21 @@
+from borrowings_app.models import Borrowing
 from payments_app.models import Payment
 from payments_app.serializers import PaymentSerializer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
-class PaymentViewSet(ModelViewSet):
+class PaymentViewSet(ReadOnlyModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_superuser:
+
+        if user.is_staff:
             return Payment.objects.all()
-        return Payment.objects.filter(borrowing_id=user.pk)
+
+        user_borrowings_ids = Borrowing.objects.filter(
+            user=user
+        ).values_list('id', flat=True)
+
+        return Payment.objects.filter(borrowing_id__in=user_borrowings_ids)
