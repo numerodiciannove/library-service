0017f0cb8a0aaa7db9cb4d9bf6610c88314a2225
numer <5079447@gmail.com>
2024-04-26 05:33:57 +0300

    Added task celery for sending user notifications about overdue.

---
+++ borrowings_app/tasks.py
@@ -0,0 +1,11 @@
+from library_service.celery import app
+from utils.overdue_borrowings_text_generator import generate_all_messages
+from utils.telegram import message_sender
+
+
+@app.task()
+def send_overdue_message():
+    messages = generate_all_messages()
+
+    for message in messages:
+        message_sender(message)
