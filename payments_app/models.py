04c573ff3d178fed17b20675ab695b55317c6953
numer <5079447@gmail.com>
2024-04-30 05:24:05 +0300

    Updated Payment money_to_pay field to max_digits=10.

--- payments_app/models.py
+++ payments_app/models.py
@@ -28,7 +28,7 @@ class Payment(models.Model):
     )
     session_url = models.URLField(max_length=255)
     session_id = models.CharField(max_length=255)
-    money_to_pay = models.DecimalField(max_digits=5, decimal_places=2)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"Type: '{self.type}', " f"Status:'{self.status}'"
